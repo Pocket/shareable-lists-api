eb4ccd8bf678f4d9716d71d2bfe8f68b8d0cc69e
Katerina Chinnappan <kchinnappan@mozilla.com>
2023-02-06 15:22:44 -0800

    OSL-139: configuring Sentry & minor refactoring

---
+++ src/sample-queries.gql.ts
@@ -0,0 +1,21 @@
+import { gql } from 'graphql-tag';
+
+export const GET_LISTS = gql`
+	query lists {
+		lists {
+			id
+			title
+			owner
+			slug
+			description
+			status
+			listItems {
+				id
+				listId
+				itemId
+				title
+				excerpt
+			}
+		}
+	}
+`;
