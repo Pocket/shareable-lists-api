98f9a55cf33ceb961e76054f43a8245f6f2474b6
Eric Fixler <efixler@mozilla.com>
2023-02-21 14:26:14 -0500

    Missed one lint update

--- src/public/resolvers/index.ts
+++ src/public/resolvers/index.ts
@@ -1,10 +1,10 @@
 import { getShareableList, getShareableLists } from './queries/ShareableList';
-import { 
+import {
   createShareableList,
   deleteShareableList,
   updateShareableList,
 } from './mutations/ShareableList';
-import { deleteShareableListItem } from './mutations/ShareableListItem'
+import { deleteShareableListItem } from './mutations/ShareableListItem';
 
 import { shareableListFieldResolvers } from './fieldResolvers';
 
