f16a59b8df2e73cc49538e5b8d157bd01598088c
Katerina Chinnappan <kchinnappan@mozilla.com>
2023-03-02 14:59:46 -0800

    OSL-145: adding snowplow events unit tests

---
+++ src/snowplow/events.spec.ts
@@ -0,0 +1,395 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import * as Sentry from '@sentry/node';
+import { EventBridgeClient } from '@aws-sdk/client-eventbridge';
+import { ListStatus, ModerationStatus } from '@prisma/client';
+import { ShareableListComplete, ShareableListItem } from '../database/types';
+import {
+  generateShareableListEventBridgePayload,
+  generateShareableListItemEventBridgePayload,
+  sendEvent,
+  sendEventHelper,
+} from './events';
+import {
+  EventBridgeEventType,
+  EventBridgeEventOptions,
+  SnowplowShareableList,
+  SnowplowShareableListItem,
+  ShareableListEventBusPayload,
+  ShareableListItemEventBusPayload,
+} from './types';
+import { faker } from '@faker-js/faker';
+
+describe('Snowplow event helpers', () => {
+  let s3Stub: sinon.SinonStub;
+  let sentryStub;
+  let crumbStub;
+  let consoleSpy;
+
+  const shareableList: ShareableListComplete = {
+    externalId: faker.datatype.uuid(),
+    userId: BigInt(12345),
+    slug: null,
+    title: 'Fake Random Title',
+    description: faker.lorem.sentences(2),
+    status: ListStatus.PRIVATE,
+    moderationStatus: ModerationStatus.VISIBLE,
+    moderatedBy: null,
+    moderationReason: null,
+    createdAt: new Date('2023-01-01 10:10:10'),
+    updatedAt: new Date('2023-01-01 10:10:10'),
+  };
+
+  const shareableListItem: ShareableListItem = {
+    // externalId: faker.datatype.uuid(),
+    itemId: BigInt(98765),
+    url: `${faker.internet.url()}/${faker.lorem.slug(5)}`,
+    title: faker.random.words(5),
+    excerpt: faker.lorem.sentences(2),
+    imageUrl: faker.image.cats(),
+    publisher: faker.company.name(),
+    authors: `${faker.name.fullName()},${faker.name.fullName()}`,
+    sortOrder: faker.datatype.number(),
+    createdAt: new Date('2023-01-01 10:10:10'),
+    updatedAt: new Date('2023-01-01 10:10:10'),
+  };
+  const shareableListItemExternalId = faker.datatype.uuid();
+
+  beforeEach(() => {
+    // we mock the send method on EventBridgeClient
+    s3Stub = sinon
+      .stub(EventBridgeClient.prototype, 'send')
+      .resolves({ FailedEntryCount: 0 });
+    sentryStub = sinon.stub(Sentry, 'captureException').resolves();
+    consoleSpy = sinon.spy(console, 'log');
+    crumbStub = sinon.stub(Sentry, 'addBreadcrumb').resolves();
+  });
+
+  afterEach(() => {
+    s3Stub.restore();
+    sentryStub.restore();
+    consoleSpy.restore();
+    crumbStub.restore();
+  });
+
+  it('generateShareableListEventBridgePayload function', async () => {
+    let payload = await generateShareableListEventBridgePayload(
+      EventBridgeEventType.SHAREABLE_LIST_CREATED,
+      shareableList
+    );
+    // shareableList obj must not be null
+    expect(payload.shareableList).to.not.be.null;
+    // check that the payload event type is for shareable-list-created
+    expect(payload.eventType).to.equal(
+      EventBridgeEventType.SHAREABLE_LIST_CREATED
+    );
+    // now check that API obj properties have been mapped to the Snowplow obj properties
+
+    // externalId -> shareable_list_external_id
+    expect(payload.shareableList.shareable_list_external_id).to.equal(
+      shareableList.externalId
+    );
+    // moderationStatus -> moderation_status
+    expect(payload.shareableList.moderation_status).to.equal(
+      shareableList.moderationStatus
+    );
+    // moderatedBy -> moderated_by
+    expect(payload.shareableList.moderated_by).to.equal(undefined);
+    // moderationReason -> moderation_reason
+    expect(payload.shareableList.moderation_reason).to.equal(undefined);
+    // createdAt -> created_at in unix timestamp
+    expect(payload.shareableList.created_at).to.equal(
+      Math.floor(shareableList.createdAt.getTime() / 1000)
+    );
+    // updatedAt -> updated_at in unix timestamp
+    expect(payload.shareableList.updated_at).to.equal(
+      Math.floor(shareableList.updatedAt.getTime() / 1000)
+    );
+
+    // update some properties
+    shareableList.title = 'Updated random title';
+    shareableList.description = 'updated description';
+    shareableList.updatedAt = new Date('2023-02-01 10:15:15');
+    payload = await generateShareableListEventBridgePayload(
+      EventBridgeEventType.SHAREABLE_LIST_UPDATED,
+      shareableList
+    );
+    // shareableList obj must not be null
+    expect(payload.shareableList).to.not.be.null;
+    // check that the payload event type is for shareable-list-updated
+    expect(payload.eventType).to.equal(
+      EventBridgeEventType.SHAREABLE_LIST_UPDATED
+    );
+    // check that title was updated
+    expect(payload.shareableList.title).to.equal('Updated random title');
+    // check that description was updated
+    expect(payload.shareableList.description).to.equal('updated description');
+    // updatedAt -> updated_at in second == 1675275315
+    expect(payload.shareableList.updated_at).to.equal(1675275315);
+
+    // update some properties
+    shareableList.status = ListStatus.PUBLIC;
+    shareableList.updatedAt = new Date('2023-02-01 10:15:45');
+    payload = await generateShareableListEventBridgePayload(
+      EventBridgeEventType.SHAREABLE_LIST_PUBLISHED,
+      shareableList
+    );
+    // shareableList obj must not be null
+    expect(payload.shareableList).to.not.be.null;
+    // check that the payload event type is for shareable-list-published
+    expect(payload.eventType).to.equal(
+      EventBridgeEventType.SHAREABLE_LIST_PUBLISHED
+    );
+    // check that status was updated to PUBLIC
+    expect(payload.shareableList.status).to.equal(ListStatus.PUBLIC);
+    // updatedAt -> updated_at in second == 1675275345
+    expect(payload.shareableList.updated_at).to.equal(1675275345);
+
+    // update some properties
+    shareableList.status = ListStatus.PRIVATE;
+    shareableList.updatedAt = new Date('2023-02-02 10:15:07');
+    payload = await generateShareableListEventBridgePayload(
+      EventBridgeEventType.SHAREABLE_LIST_UNPUBLISHED,
+      shareableList
+    );
+    // shareableList obj must not be null
+    expect(payload.shareableList).to.not.be.null;
+    // check that the payload event type is for shareable-list-unpublished
+    expect(payload.eventType).to.equal(
+      EventBridgeEventType.SHAREABLE_LIST_UNPUBLISHED
+    );
+    // check that status was updated to PUBLIC
+    expect(payload.shareableList.status).to.equal(ListStatus.PRIVATE);
+    // updatedAt -> updated_at in second == 1675361707
+    expect(payload.shareableList.updated_at).to.equal(1675361707);
+
+    // simulate shareable-list-deleted event
+    payload = await generateShareableListEventBridgePayload(
+      EventBridgeEventType.SHAREABLE_LIST_DELETED,
+      shareableList
+    );
+    // shareableList obj must not be null
+    expect(payload.shareableList).to.not.be.null;
+    // check that the payload event type is for shareable-list-deleted
+    expect(payload.eventType).to.equal(
+      EventBridgeEventType.SHAREABLE_LIST_DELETED
+    );
+
+    // TO-DO: after https://getpocket.atlassian.net/browse/OSL-126 is complete, test for shareable-list-hidden event
+  });
+
+  it('generateShareableListItemEventBridgePayload function', async () => {
+    let payload = await generateShareableListItemEventBridgePayload(
+      EventBridgeEventType.SHAREABLE_LIST_ITEM_CREATED,
+      shareableListItem,
+      shareableListItemExternalId,
+      shareableList.externalId
+    );
+    // shareableListItem obj must not be null
+    expect(payload.shareableListItem).to.not.be.null;
+    // check that the payload event type is for shareable-list-item-created
+    expect(payload.eventType).to.equal(
+      EventBridgeEventType.SHAREABLE_LIST_ITEM_CREATED
+    );
+    // now check that API obj properties have been mapped to the Snowplow obj properties
+
+    // externalId -> shareable_list_item_external_id
+    expect(payload.shareableListItem.shareable_list_item_external_id).to.equal(
+      shareableListItemExternalId
+    );
+    // listId -> shareable_list_external_id
+    expect(payload.shareableListItem.shareable_list_external_id).to.equal(
+      shareableList.externalId
+    );
+    // url-> given_url
+    expect(payload.shareableListItem.given_url).to.equal(shareableListItem.url);
+    // imageUrl-> image_url
+    expect(payload.shareableListItem.image_url).to.equal(
+      shareableListItem.imageUrl
+    );
+    // authors string getting mapped to array of strings
+    expect(JSON.stringify(payload.shareableListItem.authors)).to.equal(
+      JSON.stringify(shareableListItem.authors.split(','))
+    );
+    // createdAt -> created_at in unix timestamp
+    expect(payload.shareableListItem.created_at).to.equal(
+      Math.floor(shareableListItem.createdAt.getTime() / 1000)
+    );
+    // updatedAt -> updated_at in unix timestamp
+    expect(payload.shareableListItem.updated_at).to.equal(
+      Math.floor(shareableListItem.updatedAt.getTime() / 1000)
+    );
+
+    // simulate shareable-list-item-deleted event
+    payload = await generateShareableListItemEventBridgePayload(
+      EventBridgeEventType.SHAREABLE_LIST_ITEM_DELETED,
+      shareableListItem,
+      shareableListItemExternalId,
+      shareableList.externalId
+    );
+    // shareableList obj must not be null
+    expect(payload.shareableListItem).to.not.be.null;
+    // check that the payload event type is for shareable-list-item-deleted
+    expect(payload.eventType).to.equal(
+      EventBridgeEventType.SHAREABLE_LIST_ITEM_DELETED
+    );
+  });
+  describe('sendEventHelper function', () => {
+    it('should log error if send call throws error for shareable-list event', async () => {
+      s3Stub.restore();
+      s3Stub = sinon
+        .stub(EventBridgeClient.prototype, 'send')
+        .rejects(new Error('boo!'));
+
+      // pass shareable-list event as example
+      await sendEventHelper(EventBridgeEventType.SHAREABLE_LIST_CREATED, {
+        shareableList,
+        isShareableListEventType: true,
+      });
+
+      // Wait just a tad in case promise needs time to resolve
+      await setTimeout(() => {
+        // nothing to see here
+      }, 100);
+      expect(sentryStub.callCount).to.equal(1);
+      expect(sentryStub.getCall(0).firstArg.message).to.contain('boo!');
+      expect(crumbStub.callCount).to.equal(1);
+      expect(crumbStub.getCall(0).firstArg.message).to.contain(
+        `Failed to send event 'shareable-list-created' to event bus`
+      );
+      expect(consoleSpy.callCount).to.equal(2);
+      expect(consoleSpy.getCall(0).firstArg.message).to.contain(
+        `Failed to send event 'shareable-list-created' to event bus`
+      );
+    });
+    it('should log error if send call throws error for shareable-list-item event', async () => {
+      s3Stub.restore();
+      s3Stub = sinon
+        .stub(EventBridgeClient.prototype, 'send')
+        .rejects(new Error('boo!'));
+
+      await sendEventHelper(EventBridgeEventType.SHAREABLE_LIST_ITEM_CREATED, {
+        shareableListItem,
+        shareableListItemExternalId,
+        listExternalId: shareableList.externalId,
+        isShareableListItemEventType: true,
+      });
+
+      // Wait just a tad in case promise needs time to resolve
+      await setTimeout(() => {
+        // nothing to see here
+      }, 100);
+      expect(sentryStub.callCount).to.equal(1);
+      expect(sentryStub.getCall(0).firstArg.message).to.contain('boo!');
+      expect(crumbStub.callCount).to.equal(1);
+      expect(crumbStub.getCall(0).firstArg.message).to.contain(
+        `Failed to send event 'shareable-list-item-created' to event bus`
+      );
+      expect(consoleSpy.callCount).to.equal(2);
+      console.log(consoleSpy.getCall(0).firstArg.message);
+      expect(consoleSpy.getCall(0).firstArg.message).to.contain(
+        `Failed to send event 'shareable-list-item-created' to event bus`
+      );
+    });
+  });
+  describe('sendEvent function', () => {
+    it('should send shareable-list event to event bus with proper event data', async () => {
+      // let's first generate a payload to send to the event bridge
+      let payload = await generateShareableListEventBridgePayload(
+        EventBridgeEventType.SHAREABLE_LIST_CREATED,
+        shareableList
+      );
+      // shareableList obj must not be null
+      expect(payload.shareableList).to.not.be.null;
+      // send shareable-list event
+      await sendEvent(payload, true, false);
+
+      // // Wait just a tad in case promise needs time to resolve
+      await setTimeout(() => {
+        // nothing to see here
+      }, 100);
+      expect(sentryStub.callCount).to.equal(0);
+      expect(consoleSpy.callCount).to.equal(0);
+    });
+    it('should send shareable-list-item event to event bus with proper event data', async () => {
+      // let's first generate a payload to send to the event bridge
+      let payload = await generateShareableListItemEventBridgePayload(
+        EventBridgeEventType.SHAREABLE_LIST_ITEM_CREATED,
+        shareableListItem,
+        shareableListItemExternalId,
+        shareableList.externalId
+      );
+      // shareableList obj must not be null
+      expect(payload.shareableListItem).to.not.be.null;
+      // send shareable-list event
+      await sendEvent(payload, false, true);
+
+      // // Wait just a tad in case promise needs time to resolve
+      await setTimeout(() => {
+        // nothing to see here
+      }, 100);
+      expect(sentryStub.callCount).to.equal(0);
+      expect(consoleSpy.callCount).to.equal(0);
+    });
+    it('should log error if send call throws error for shareable-list event', async () => {
+      s3Stub.restore();
+      s3Stub = sinon
+        .stub(EventBridgeClient.prototype, 'send')
+        .resolves({ FailedEntryCount: 1 });
+
+      // let's first generate a payload to send to the event bridge
+      let payload = await generateShareableListEventBridgePayload(
+        EventBridgeEventType.SHAREABLE_LIST_CREATED,
+        shareableList
+      );
+      // shareableList obj must not be null
+      expect(payload.shareableList).to.not.be.null;
+      // send shareable-list event
+      await sendEvent(payload, true, false);
+
+      // // Wait just a tad in case promise needs time to resolve
+      await setTimeout(() => {
+        // nothing to see here
+      }, 100);
+      expect(sentryStub.callCount).to.equal(1);
+      expect(sentryStub.getCall(0).firstArg.message).to.contain(
+        `Failed to send event 'shareable-list-created' to event bus`
+      );
+      expect(consoleSpy.callCount).to.equal(1);
+      expect(consoleSpy.getCall(0).firstArg.message).to.contain(
+        `Failed to send event 'shareable-list-created' to event bus`
+      );
+    });
+    it('should log error if send call throws error for shareable-list-item event', async () => {
+      s3Stub.restore();
+      s3Stub = sinon
+        .stub(EventBridgeClient.prototype, 'send')
+        .resolves({ FailedEntryCount: 1 });
+
+      // let's first generate a payload to send to the event bridge
+      let payload = await generateShareableListItemEventBridgePayload(
+        EventBridgeEventType.SHAREABLE_LIST_ITEM_CREATED,
+        shareableListItem,
+        shareableListItemExternalId,
+        shareableList.externalId
+      );
+      // shareableListItem obj must not be null
+      expect(payload.shareableListItem).to.not.be.null;
+      // send shareable-list-item event
+      await sendEvent(payload, false, true);
+
+      // // Wait just a tad in case promise needs time to resolve
+      await setTimeout(() => {
+        // nothing to see here
+      }, 100);
+      expect(sentryStub.callCount).to.equal(1);
+      expect(sentryStub.getCall(0).firstArg.message).to.contain(
+        `Failed to send event 'shareable-list-item-created' to event bus`
+      );
+      expect(consoleSpy.callCount).to.equal(1);
+      expect(consoleSpy.getCall(0).firstArg.message).to.contain(
+        `Failed to send event 'shareable-list-item-created' to event bus`
+      );
+    });
+  });
+});
--- src/snowplow/events.ts
+++ src/snowplow/events.ts
@@ -1,18 +1,18 @@
 import {
-  PutEventsCommand,
-  PutEventsCommandOutput,
+	PutEventsCommand,
+	PutEventsCommandOutput,
 } from '@aws-sdk/client-eventbridge';
 import * as Sentry from '@sentry/node';
 import config from '../config/';
 import { eventBridgeClient } from '../aws/eventBridgeClient';
 import { ShareableListComplete, ShareableListItem } from '../database/types';
 import {
-  EventBridgeEventType,
-  EventBridgeEventOptions,
-  SnowplowShareableList,
-  SnowplowShareableListItem,
-  ShareableListEventBusPayload,
-  ShareableListItemEventBusPayload,
+	EventBridgeEventType,
+	EventBridgeEventOptions,
+	SnowplowShareableList,
+	SnowplowShareableListItem,
+	ShareableListEventBusPayload,
+	ShareableListItemEventBusPayload,
 } from './types';
 
 /**
@@ -20,28 +20,28 @@ import {
  * @param shareableList
  */
 async function transformAPIShareableListToSnowplowShareableList(
-  shareableList: ShareableListComplete
+	shareableList: ShareableListComplete
 ): Promise<SnowplowShareableList> {
-  return {
-    shareable_list_external_id: shareableList.externalId,
-    slug: shareableList.slug,
-    title: shareableList.title,
-    description: shareableList.description
-      ? shareableList.description
-      : undefined,
-    status: shareableList.status,
-    moderation_status: shareableList.moderationStatus,
-    moderated_by: shareableList.moderatedBy
-      ? shareableList.moderatedBy
-      : undefined,
-    moderation_reason: shareableList.moderationReason
-      ? shareableList.moderationReason
-      : undefined,
-    created_at: shareableList.createdAt.getSeconds(),
-    updated_at: shareableList.updatedAt
-      ? shareableList.updatedAt.getSeconds()
-      : undefined,
-  };
+	return {
+		shareable_list_external_id: shareableList.externalId,
+		slug: shareableList.slug,
+		title: shareableList.title,
+		description: shareableList.description
+			? shareableList.description
+			: undefined,
+		status: shareableList.status,
+		moderation_status: shareableList.moderationStatus,
+		moderated_by: shareableList.moderatedBy
+			? shareableList.moderatedBy
+			: undefined,
+		moderation_reason: shareableList.moderationReason
+			? shareableList.moderationReason
+			: undefined,
+		created_at: Math.floor(shareableList.createdAt.getTime() / 1000),
+		updated_at: shareableList.updatedAt
+			? Math.floor(shareableList.updatedAt.getTime() / 1000)
+			: undefined,
+	};
 }
 
 /**
@@ -49,31 +49,31 @@ async function transformAPIShareableListToSnowplowShareableList(
  * @param shareableListItem
  */
 async function transformAPIShareableListItemToSnowplowShareableListItem(
-  shareableListItem: ShareableListItem,
-  externalId: string,
-  listExternalId: string
+	shareableListItem: ShareableListItem,
+	externalId: string,
+	listExternalId: string
 ): Promise<SnowplowShareableListItem> {
-  return {
-    shareable_list_item_external_id: externalId,
-    shareable_list_external_id: listExternalId,
-    given_url: shareableListItem.url,
-    title: shareableListItem.title ? shareableListItem.title : undefined,
-    excerpt: shareableListItem.excerpt ? shareableListItem.excerpt : undefined,
-    image_url: shareableListItem.imageUrl
-      ? shareableListItem.imageUrl
-      : undefined,
-    authors: shareableListItem.authors
-      ? shareableListItem.authors.split(',')
-      : undefined,
-    publisher: shareableListItem.publisher
-      ? shareableListItem.publisher
-      : undefined,
-    sort_order: shareableListItem.sortOrder,
-    created_at: shareableListItem.createdAt.getSeconds(),
-    updated_at: shareableListItem.updatedAt
-      ? shareableListItem.updatedAt.getSeconds()
-      : undefined,
-  };
+	return {
+		shareable_list_item_external_id: externalId,
+		shareable_list_external_id: listExternalId,
+		given_url: shareableListItem.url,
+		title: shareableListItem.title ? shareableListItem.title : undefined,
+		excerpt: shareableListItem.excerpt ? shareableListItem.excerpt : undefined,
+		image_url: shareableListItem.imageUrl
+			? shareableListItem.imageUrl
+			: undefined,
+		authors: shareableListItem.authors
+			? shareableListItem.authors.split(',')
+			: undefined,
+		publisher: shareableListItem.publisher
+			? shareableListItem.publisher
+			: undefined,
+		sort_order: shareableListItem.sortOrder,
+		created_at: Math.floor(shareableListItem.createdAt.getTime() / 1000),
+		updated_at: shareableListItem.updatedAt
+			? Math.floor(shareableListItem.updatedAt.getTime() / 1000)
+			: undefined,
+	};
 }
 
 /**
@@ -82,16 +82,16 @@ async function transformAPIShareableListItemToSnowplowShareableListItem(
  * @param eventType
  * @param shareableList
  */
-async function generateShareableListEventBridgePayload(
-  eventType: EventBridgeEventType,
-  shareableList: ShareableListComplete
+export async function generateShareableListEventBridgePayload(
+	eventType: EventBridgeEventType,
+	shareableList: ShareableListComplete
 ): Promise<ShareableListEventBusPayload> {
-  return {
-    shareableList: await transformAPIShareableListToSnowplowShareableList(
-      shareableList
-    ),
-    eventType: eventType,
-  };
+	return {
+		shareableList: await transformAPIShareableListToSnowplowShareableList(
+			shareableList
+		),
+		eventType: eventType,
+	};
 }
 
 /**
@@ -102,21 +102,21 @@ async function generateShareableListEventBridgePayload(
  * @param externalId
  * @param listExternalId
  */
-async function generateShareableListItemEventBridgePayload(
-  eventType: EventBridgeEventType,
-  shareableListItem: ShareableListItem,
-  externalId: string,
-  listExternalId: string
+export async function generateShareableListItemEventBridgePayload(
+	eventType: EventBridgeEventType,
+	shareableListItem: ShareableListItem,
+	externalId: string,
+	listExternalId: string
 ): Promise<ShareableListItemEventBusPayload> {
-  return {
-    shareableListItem:
-      await transformAPIShareableListItemToSnowplowShareableListItem(
-        shareableListItem,
-        externalId,
-        listExternalId
-      ),
-    eventType: eventType,
-  };
+	return {
+		shareableListItem:
+			await transformAPIShareableListItemToSnowplowShareableListItem(
+				shareableListItem,
+				externalId,
+				listExternalId
+			),
+		eventType: eventType,
+	};
 }
 
 /**
@@ -126,49 +126,48 @@ async function generateShareableListItemEventBridgePayload(
  * @param options
  */
 export async function sendEventHelper(
-  eventType: EventBridgeEventType,
-  options: EventBridgeEventOptions
+	eventType: EventBridgeEventType,
+	options: EventBridgeEventOptions
 ) {
-  let payload;
+	let payload;
 
-  if (options.shareableList) {
-    payload = await generateShareableListEventBridgePayload(
-      eventType,
-      options.shareableList
-    );
-  }
+	if (options.shareableList) {
+		payload = await generateShareableListEventBridgePayload(
+			eventType,
+			options.shareableList
+		);
+	}
 
-  if (options.shareableListItem) {
-    payload = await generateShareableListItemEventBridgePayload(
-      eventType,
-      options.shareableListItem,
-      options.shareableListItemExternalId,
-      options.listExternalId
-    );
-  }
-  console.log('payload: ', payload);
+	if (options.shareableListItem) {
+		payload = await generateShareableListItemEventBridgePayload(
+			eventType,
+			options.shareableListItem,
+			options.shareableListItemExternalId,
+			options.listExternalId
+		);
+	}
 
-  // Send payload to Event Bridge.
-  try {
-    await sendEvent(
-      payload,
-      options.isShareableListEventType,
-      options.isShareableListItemEventType
-    );
-  } catch (error) {
-    // In the unlikely event that the payload generator throws an error,
-    // log to Sentry and Cloudwatch but don't halt program
-    const failedEventError = new Error(
-      `Failed to send event '${
-        payload.eventType
-      }' to event bus. Event Body:\n ${JSON.stringify(payload)}`
-    );
-    // Don't halt program, but capture the failure in Sentry and Cloudwatch
-    Sentry.addBreadcrumb(failedEventError);
-    Sentry.captureException(error);
-    console.log(failedEventError);
-    console.log(error);
-  }
+	// Send payload to Event Bridge.
+	try {
+		await sendEvent(
+			payload,
+			options.isShareableListEventType,
+			options.isShareableListItemEventType
+		);
+	} catch (error) {
+		// In the unlikely event that the payload generator throws an error,
+		// log to Sentry and Cloudwatch but don't halt program
+		const failedEventError = new Error(
+			`Failed to send event '${
+				payload.eventType
+			}' to event bus. Event Body:\n ${JSON.stringify(payload)}`
+		);
+		// Don't halt program, but capture the failure in Sentry and Cloudwatch
+		Sentry.addBreadcrumb(failedEventError);
+		Sentry.captureException(error);
+		console.log(failedEventError);
+		console.log(error);
+	}
 }
 
 /**
@@ -181,46 +180,42 @@ export async function sendEventHelper(
  * @param eventPayload the payload to send to event bus
  */
 export async function sendEvent(
-  eventPayload: any,
-  isShareableListEventType: boolean,
-  isShareableListItemEventType: boolean
+	eventPayload: any,
+	isShareableListEventType: boolean,
+	isShareableListItemEventType: boolean
 ) {
-  let eventBridgeSource;
-  if (isShareableListEventType) {
-    eventBridgeSource = config.aws.eventBus.eventBridge.shareableList.source;
-  }
-  if (isShareableListItemEventType) {
-    eventBridgeSource =
-      config.aws.eventBus.eventBridge.shareableListItem.source;
-  }
-  const putEventCommand = new PutEventsCommand({
-    Entries: [
-      {
-        EventBusName: config.aws.eventBus.name,
-        Detail: JSON.stringify(eventPayload),
-        Source: eventBridgeSource,
-        DetailType: eventPayload.eventType,
-      },
-    ],
-  });
+	let eventBridgeSource;
+	if (isShareableListEventType) {
+		eventBridgeSource = config.aws.eventBus.eventBridge.shareableList.source;
+	}
+	if (isShareableListItemEventType) {
+		eventBridgeSource =
+			config.aws.eventBus.eventBridge.shareableListItem.source;
+	}
+	const putEventCommand = new PutEventsCommand({
+		Entries: [
+			{
+				EventBusName: config.aws.eventBus.name,
+				Detail: JSON.stringify(eventPayload),
+				Source: eventBridgeSource,
+				DetailType: eventPayload.eventType,
+			},
+		],
+	});
 
-  console.log('putEventCommand: ', putEventCommand.input);
+	const output: PutEventsCommandOutput = await eventBridgeClient.send(
+		putEventCommand
+	);
 
-  const output: PutEventsCommandOutput = await eventBridgeClient.send(
-    putEventCommand
-  );
+	if (output.FailedEntryCount) {
+		const failedEventError = new Error(
+			`Failed to send event '${
+				eventPayload.eventType
+			}' to event bus. Event Body:\n ${JSON.stringify(eventPayload)}`
+		);
 
-  console.log('output: ', output);
-
-  if (output.FailedEntryCount) {
-    const failedEventError = new Error(
-      `Failed to send event '${
-        eventPayload.eventType
-      }' to event bus. Event Body:\n ${JSON.stringify(eventPayload)}`
-    );
-
-    // Don't halt program, but capture the failure in Sentry and Cloudwatch
-    Sentry.captureException(failedEventError);
-    console.log(failedEventError);
-  }
+		// Don't halt program, but capture the failure in Sentry and Cloudwatch
+		Sentry.captureException(failedEventError);
+		console.log(failedEventError);
+	}
 }
