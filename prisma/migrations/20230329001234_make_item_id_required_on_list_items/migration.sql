33d072ddd1bfd18e25a02295476e0d9d437d0712
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2023-03-30 12:02:08 +1100

    Revert changes to Prisma

--- prisma/migrations/20230329001234_make_item_id_required_on_list_items/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `itemId` on table `ListItem` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `ListItem` MODIFY `itemId` BIGINT NOT NULL;
