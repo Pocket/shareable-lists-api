db3d70564796ee30611c1e4ebeab8aa5a61f4619
Katerina Chinnappan <kchinnappan@mozilla.com>
2023-04-12 13:43:06 -0700

    addressing feedback

--- src/public/routes/deleteUserData.integration.ts
+++ src/public/routes/deleteUserData.integration.ts
@@ -14,6 +14,10 @@ import {
   createShareableListItemHelper,
   mockRedisServer,
 } from '../../test/helpers';
+import {
+  getAllShareableListIdsForUser,
+  deleteShareableListItemsForUser,
+} from './deleteUserData';
 
 describe('/deleteUserData express endpoint', () => {
   let app: Express.Application;
@@ -21,7 +25,11 @@ describe('/deleteUserData express endpoint', () => {
   let graphQLUrl: string;
   let db: PrismaClient;
   let sentryStub;
+  let list1;
+  let list2;
+  let list3;
 
+  let pilotUser2;
   const headers = {
     userId: '8009882300',
   };
@@ -54,33 +62,40 @@ describe('/deleteUserData express endpoint', () => {
     await createPilotUserHelper(db, {
       userId: parseInt(headers.userId),
     });
+
+    pilotUser2 = await createPilotUserHelper(db, {
+      userId: 76543,
+    });
+
+    // Create a few Lists
+    list1 = await createShareableListHelper(db, {
+      userId: parseInt(headers.userId),
+      title: 'Simon Le Bon List',
+    });
+
+    list2 = await createShareableListHelper(db, {
+      userId: parseInt(headers.userId),
+      title: 'Bon Voyage List',
+    });
+
+    // Create a List for user 2
+    list3 = await createShareableListHelper(db, {
+      userId: pilotUser2.userId,
+      title: 'Rolling Stones List',
+    });
+
+    // then create 5 list items for each list
+    for (let i = 0; i < 5; i++) {
+      await createShareableListItemHelper(db, {
+        list: list1,
+      });
+      await createShareableListItemHelper(db, {
+        list: list2,
+      });
+    }
   });
 
   describe('should delete all shareable list user data for userId', () => {
-    beforeEach(async () => {
-      // Create a few Lists
-      const list1 = await createShareableListHelper(db, {
-        userId: parseInt(headers.userId),
-        title: 'Simon Le Bon List',
-      });
-
-      const list2 = await createShareableListHelper(db, {
-        userId: parseInt(headers.userId),
-        title: 'Bon Voyage List',
-      });
-
-      // then create some random # of list items for each list
-      const makeItems = Math.floor(Math.random() * 4) + 1;
-      for (let i = 0; i < makeItems; i++) {
-        await createShareableListItemHelper(db, {
-          list: list1,
-        });
-        await createShareableListItemHelper(db, {
-          list: list2,
-        });
-      }
-    });
-
     it('should not delete any data for userId with no data and should not return error', async () => {
       const result = await request(app)
         .post(graphQLUrl + 'deleteUserData')
@@ -110,6 +125,49 @@ describe('/deleteUserData express endpoint', () => {
       expect(result.body.message).to.contain(
         `Deleting shareable lists data for User ID: ${headers.userId}`
       );
+      // lets manually call getAllShareableListIdsForUser to check there are no lists for this user
+      const ids = await getAllShareableListIdsForUser(parseInt(headers.userId));
+      expect(ids.length).to.equal(0);
+    });
+  });
+
+  describe('getAllShareableListIdsForUser', () => {
+    it('should return empty array for userId with no lists in db', async () => {
+      const ids = await getAllShareableListIdsForUser(234567);
+      expect(ids.length).to.equal(0);
+    });
+    it('should return appropriate list ids for user', async () => {
+      let ids = await getAllShareableListIdsForUser(parseInt(headers.userId));
+      expect(ids.length).to.equal(2);
+      // check the returned ids are what we expect
+      expect(ids[0]).to.equal(parseInt(list1.id as unknown as string));
+      expect(ids[1]).to.equal(parseInt(list2.id as unknown as string));
+
+      // get listIds for another user
+      ids = await getAllShareableListIdsForUser(pilotUser2.userId);
+      expect(ids.length).to.equal(1);
+      // check the returned ids are what we expect
+      expect(ids[0]).to.equal(parseInt(list3.id as unknown as string));
+    });
+  });
+
+  describe('deleteShareableListItemsForUser', () => {
+    it('should return count=0 for deleting 0 list items for user with no list items', async () => {
+      // pilotUser 2 has one list (list3) but 0 list items
+      const count = await deleteShareableListItemsForUser([
+        parseInt(list3.id as unknown as string),
+      ]);
+      // there should be no list items found for list 3
+      expect(count).to.equal(0);
+    });
+    it('should return correct count of deleted list items for user with list items', async () => {
+      // pilotUser 1 has two lists (list1, list2) and 5 list items per list
+      const count = await deleteShareableListItemsForUser([
+        parseInt(list1.id as unknown as string),
+        parseInt(list2.id as unknown as string),
+      ]);
+      // there should be 10 total list items deleted
+      expect(count).to.equal(10);
     });
   });
 });
--- src/public/routes/deleteUserData.ts
+++ src/public/routes/deleteUserData.ts
@@ -44,20 +44,18 @@ function validate(req: Request, res: Response, next: NextFunction) {
  * This method returns an array of shareable list ids for a user
  * @param userId
  */
-async function getAllShareableListIdsForUser(
+export async function getAllShareableListIdsForUser(
   userId: number | bigint
 ): Promise<number[] | bigint[]> {
   const shareableLists = await db.list.findMany({
     where: {
       userId,
     },
-    orderBy: { updatedAt: 'desc' },
-    include: {
-      listItems: true,
-    },
   });
   const shareableListIds = [];
   shareableLists.forEach(function (list) {
+    // db spits list.id as 1n (bigint) but needs to be interpreted as string in order
+    // to apply parseInt which accepts a str argument, hence, double casting
     shareableListIds.push(parseInt(list.id as unknown as string));
   });
   return shareableListIds;
@@ -67,7 +65,7 @@ async function getAllShareableListIdsForUser(
  * This method deletes shareable list items in bulk for a user
  * @param listIds
  */
-async function deleteShareableListItemsForUser(
+export async function deleteShareableListItemsForUser(
   listIds: number[] | bigint[]
 ): Promise<number> {
   const batchResult = await db.listItem.deleteMany({
@@ -91,23 +89,19 @@ async function deleteShareableListUserData(
   // to the db
   if (shareableListIds.length === 0) {
     return `No shareable list data to delete for User ID: ${userId} (requestId='${requestId}')`;
-  } else if (shareableListIds.length > 0) {
-    // First, delete all list items if there are any
-    await deleteShareableListItemsForUser(shareableListIds);
-    // Now, delete all lists
-    await db.list
-      .deleteMany({
-        where: { userId },
-      })
-      .catch((error) => {
-        // some unexpected DB error, log to Sentry, but don't halt program
-        Sentry.captureException(
-          'Failed to delete shareable list data: ',
-          error
-        );
-      });
-    return `Deleting shareable lists data for User ID: ${userId} (requestId='${requestId}')`;
   }
+  // First, delete all list items if there are any
+  await deleteShareableListItemsForUser(shareableListIds);
+  // Now, delete all lists
+  await db.list
+    .deleteMany({
+      where: { userId },
+    })
+    .catch((error) => {
+      // some unexpected DB error, log to Sentry, but don't halt program
+      Sentry.captureException('Failed to delete shareable list data: ', error);
+    });
+  return `Deleting shareable lists data for User ID: ${userId} (requestId='${requestId}')`;
 }
 
 router.post(
@@ -129,11 +123,11 @@ router.post(
       .catch((e) => {
         // In the unlikely event that an error is thrown,
         // log to Sentry but don't halt program
+        Sentry.captureException(e);
         return res.send({
           status: 'BAD_REQUEST',
           message: e,
         });
-        Sentry.captureException(e);
       });
   }
 );
