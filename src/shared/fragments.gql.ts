07d135c935a750a8ed9e366aae72cd46793957cc
Jonathan Petto <jpetto@users.noreply.github.com>
2023-05-12 11:44:46 -0500

    chore(shareableListPublic): add listItemNoteVisibility to query

--- src/shared/fragments.gql.ts
+++ src/shared/fragments.gql.ts
@@ -69,6 +69,7 @@ export const ShareableListPublicQueryProps = gql`
     listItems {
       ...ShareableListItemPublicProps
     }
+    listItemNoteVisibility
   }
   ${ShareableListItemPublicProps}
 `;
